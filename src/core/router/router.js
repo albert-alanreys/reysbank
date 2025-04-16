0ce7ef65408ac405301c63eeb8f67c2d748b2124
Albert Alan-Reys <alanreys@mail.ru>
2025-04-16 19:56:53 +1000

    Implement basic layout component

--- src/core/router/router.js
+++ src/core/router/router.js
@@ -1,34 +1,41 @@
 import { ROUTES } from './routes.data';
+import { Layout } from '@/components/layout/layout.component';
 
 export class Router {
+	#layout;
 	#routes;
 	#currentRoute;
 
 	constructor() {
 		this.#routes = ROUTES;
-		this.#currentRoute = null;
-
 		this.#handleRouteChange();
 	}
 
-	getCurrentPath() {
-		return globalThis.location.pathname;
-	}
-
 	#handleRouteChange() {
-		const path = this.getCurrentPath() ?? '/';
+		const path = this.#getCurrentPath() ?? '/';
 		const route =
 			this.#routes.find((route) => route.path === path) ??
 			this.#routes.find((route) => route.path === '/not-found');
 
 		this.#currentRoute = route;
-		this.render();
+		this.#render();
 	}
 
-	render() {
-		console.log(this.#currentRoute);
+	#getCurrentPath() {
+		return globalThis.location.pathname;
+	}
 
+	#render() {
 		const component = new this.#currentRoute.component();
-		document.getElementById('app').innerHTML = component.render();
+
+		if (!this.#layout) {
+			this.#layout = new Layout({
+				router: this,
+				children: component.render(),
+			});
+			document.getElementById('app').innerHTML = this.#layout.render();
+		} else {
+			document.querySelector('main').innerHTML = component.render();
+		}
 	}
 }
