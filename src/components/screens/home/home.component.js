4a9fa7f9d3f9013b49213f00f1b3d503df39afc6
Albert Alan-Reys <alanreys@mail.ru>
2025-04-19 20:53:05 +1000

    Add UserItem component and extend RQuery

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,6 +1,7 @@
 import { BaseScreen } from '@/core/component/base-screen.component';
 import { renderService } from '@/core/services/render.service';
 import { Field } from '@/components/ui/field/field.component';
+import { UserItem } from '@/components/ui/user-item/user-item.component';
 
 import * as styles from './home.module.scss';
 import templateHTML from './home.template.html';
@@ -20,6 +21,14 @@ export class Home extends BaseScreen {
 					variant: 'credit-card',
 					type: 'type',
 				}),
+				new UserItem(
+					{
+						avatarPath: 'https://avatars.githubusercontent.com/u/181439848?v=4',
+						name: 'Albert Alan-Reys',
+					},
+					false,
+					() => alert('Hello'),
+				),
 			],
 			styles,
 		);
---
+++ src/components/ui/user-item/user-item.component.js
@@ -0,0 +1,44 @@
+import { $R } from '@/core/rquery/rquery.lib';
+import { Child } from '@/core/component/child.component';
+import { renderService } from '@/core/services/render.service';
+
+import * as styles from './user-item.module.scss';
+import templateHTML from './user-item.template.html';
+
+export class UserItem extends Child {
+	constructor(user, isGray = false, onClick = null) {
+		super();
+
+		if (!user) throw new Error('User should be passed');
+		if (!user?.name) throw new Error('User must have a "name"');
+		if (!user?.avatarPath) throw new Error('User must have an "avatarPath"');
+
+		this.user = user;
+		this.isGray = isGray;
+		this.onClick = onClick;
+	}
+
+	render() {
+		this.element = renderService.htmlToElement(templateHTML, [], styles);
+
+		this.update(this.user);
+
+		$R(this.element).click(this.onClick || this.#preventDefault.bind(this));
+
+		if (!this.onClick) $R(this.element).attr('disabled', '');
+		if (this.isGray) $R(this.element).addClass(styles.gray);
+
+		return this.element;
+	}
+
+	update({ avatarPath, name }) {
+		if (avatarPath && name) {
+			$R(this.element).find('img').attr('src', avatarPath).attr('alt', name);
+			$R(this.element).find('span').text(name);
+		}
+	}
+
+	#preventDefault(event) {
+		event.preventDefault();
+	}
+}
